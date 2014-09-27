895d1ba84bc69335e7e37885e562e0ed4ba0e641
krongk <kenrome@gmail.com>
2014-09-27 11:14:35 +0800

    updatea

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,6 +3,6 @@ Paperclip::Attachment.default_options[:qiniu_credentials] = {
   :access_key => ENV['QINIU_ACCESS_KEY'] || raise("set env QINIU_ACCESS_KEY"),
   :secret_key => ENV['QINIU_SECRET_KEY'] || raise("set env QINIU_SECRET_KEY")
 }
-Paperclip::Attachment.default_options[:bucket] = 'tm-card'
+Paperclip::Attachment.default_options[:bucket] = 'song-dev'
 Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:qiniu_host] = 'http://tm-card.qiniudn.com'
+Paperclip::Attachment.default_options[:qiniu_host] = 'http://song-dev.qiniudn.com'
